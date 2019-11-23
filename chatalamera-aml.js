2fb6d75cfaf0c8e1f115622f904453aa136c8319
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2019-11-23 18:52:11 +0300

    Create chatalamera-aml.js

---
+++ chatalamera-aml.js
@@ -0,0 +1,10 @@
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$( "button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" );
+$(".border.corner").css("border-radius","24px 24px 24px 24px");
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="https://teamalmhia1.000webhostapp.com/1wlllh.css">
+<style>
+.fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px  rgb(0,0,0), 0 0 6px rgb(216,92,140);border-radius: 4px 4px 4px 4px;;}
+textarea.fl.filw.corner.tbox {border-radius: 10px;padding-left: 5px;border: 2px solid #adadad;margin-top: 2px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 2px solid #adadad;font-family: 'jazeera',FontAwesome;border-radius: 24px 24px 24px 24px;} </style>`).insertBefore('body');
