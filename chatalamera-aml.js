f9bee84d78b51a033fcbc95dc31cd8d093fbb496
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2019-11-23 18:56:49 +0300

    Update chatalamera-aml.js

--- chatalamera-aml.js
+++ chatalamera-aml.js
@@ -3,7 +3,7 @@ $('a[href="https://jawalhost.com/"]').removeAttr('href');
 $( "button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" );
 $(".border.corner").css("border-radius","24px 24px 24px 24px");
 $(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
-<link rel="stylesheet" href="https://teamalmhia1.000webhostapp.com/1wlllh.css">
+<link rel="stylesheet" href="https://raw.githack.com/teamalmhia/-chatalamera-aml/master/chatalamera-aml.css">
 <style>
 .fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px  rgb(0,0,0), 0 0 6px rgb(216,92,140);border-radius: 4px 4px 4px 4px;;}
 textarea.fl.filw.corner.tbox {border-radius: 10px;padding-left: 5px;border: 2px solid #adadad;margin-top: 2px;}
